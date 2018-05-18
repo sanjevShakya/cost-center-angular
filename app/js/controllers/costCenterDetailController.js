6dd924eb742348b5f27bb12b9a5cbd69626a9d8f
sanjeev <sanjevsakya@gmail.com>
2018-05-18 12:33:20 +0545

    added costCenterDetailController

---
+++ app/js/controllers/costCenterDetailController.js
@@ -0,0 +1,37 @@
+app.service('CostCenterDetailService', [
+  '$http',
+  'APP_CONSTANTS',
+  function($http, APP_CONSTANTS) {
+    const cs = this;
+    const COST_CENTER_BY_ID = function(id) {
+      return APP_CONSTANTS.rootUrl + '/' + id;
+    };
+
+    cs.fetchCostCenterById = function(id) {
+      return $http.get(COST_CENTER_BY_ID(id));
+    };
+  },
+]);
+
+app.controller('CostCenterDetailController', [
+  '$http',
+  '$stateParams',
+  'CostCenterDetailService',
+  function($http, $stateParams, CostCenterDetailService) {
+    const cs = this;
+    cs.id = $stateParams.costCenterId;
+    cs.costCenterDetail = {};
+
+    const init = function() {
+      CostCenterDetailService.fetchCostCenterById(cs.id)
+        .then(function(data) {
+          cs.costCenterDetail = data.data;
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    };
+
+    init();
+  },
+]);
